9e738a768348667413e3c029935662f276f3e64c
danihuf <daniel99h@gmail.com>
2022-04-07 12:36:46 +0200

    changes in text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deployed with Azure.
         </a>
       </header>
     </div>
